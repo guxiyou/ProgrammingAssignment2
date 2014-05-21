880103d04fab7fb5d41289ef50afcf8c283009ca
Joel Gillespie <guxiyou@gmail.com>
2014-05-21 06:40:24 -0600

    complete assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -5,12 +5,44 @@
 ## makeCacheMatrix: stores initial matrix and cached inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverseM <- NULL
+    
+    setInitial <- function(init) {
+        inverseM <<- NULL
+        x <<- init
+    }
+    
+    getInitial <- function() {
+        x
+    }
+    
+    setInverse <- function(inv) {
+        inverseM <<- inv
+    }
+    
+    getInverse <- function() {
+        inverseM
+    }
+    
+    ## Return special functions
+    list(setInitial = setInitial, getInitial = getInitial,
+         setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## cacheSolve: returns the inverse matrix of a given cacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    
+    if (!is.null(inv)){
+        message("getting cached data")
+    }
+    else {
+        initial <- x$getInitial()
+        inv <- solve(initial)
+        x$setInverse(inv)
+    }
+    
+    inv  ## Return inverse matrix of 'x'
 }
